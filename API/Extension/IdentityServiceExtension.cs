bb295758ad5d06d6c19428bf3f0d75c9cb33b10a
carlpang1 <carlking0088@hotmail.com>
2022-06-28 17:07:44 +0800

    End of section 19

--- API/Extension/IdentityServiceExtension.cs
+++ API/Extension/IdentityServiceExtension.cs
@@ -40,13 +40,27 @@ namespace API.Extension
                     ValidateAudience = false
 
                 };
+                opt.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/chat")))
+                        {
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
+                }; 
             });
 
             services.AddScoped<TokenService>();
 
             services.AddAuthorization(opt =>
             {
-                opt.AddPolicy("IsActivityHost", policy=>{
+                opt.AddPolicy("IsActivityHost", policy =>
+                {
                     policy.Requirements.Add(new IsHostRequirement());
                 });
             });
